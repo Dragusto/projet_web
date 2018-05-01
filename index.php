015ecd311971314684204d8f0bada43cbd2bd05b
Vgiot14 <vgiot14@gmail.com>
2018-05-02 00:00:17 +0200

    Mise à jour index

--- index.php
+++ index.php
@@ -1,64 +1,79 @@
 <!DOCTYPE html>
+
 <html>
+
     <head>
-        <title>projet piscine</title>
+
+        <title>Projet piscine !!</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<link href="indexcss.css" rel="stylesheet" type="text/css" />
 
     </head>
+
     <body>
+
         <h1>Connection</h1>
         
-        <p>
-            Veuillez rentrer les information pour vous connecter.<br />   
-        </p>
+        <p>Veuillez vous identifier<br /></p>
 
+        <!-- Ceci permet d'afficher les champs non remplis sans changer de page -->
         <?php
-          if(isset($_GET["error_message"]))
-          {
-             $error_message = $_GET["error_message"];
+            if(isset($_GET["error_message"]))
+            {
+              $error_message = $_GET["error_message"];
         ?>
-          <p style = "color : red"> 
-          <?php echo $error_message; ?>
-          </p>
-        <?php
-          }
-        ?>
-		
+        
+        <p style = "color : red"> <?php echo $error_message; ?></p>
+          
+        <?php } ?>
+
+
+
         <form action="indextraitement.php" method="post">
-          <table id="myTable">
+
+            <div class="table">
+            <table id="myTable">
+
             <tr>
-              <td>Email</td>
+              <td>Email:</td>
               <td><input type="text" name="email"></td>
             </tr>
 
             <tr>
               <td>Mot de passe:</td>
               <td><input type="password" name="mdp"></td>
-            </tr>
-            </tr>        
+            </tr>       
               
             <tr>
-              <td colspan="10"><input type="submit" value="Soumettre"></td>
+              <td colspan="10"><input type="submit" value="Me connecter"></td>
 			  <a href="sommaire.html"></a>
             </tr>
 			
-          </table>
+            </table>
+            </div>
         </form>
 
     </body>
-			<div class="inscription">
-		<form action="inscription.php" method="post">
-		<tr>
-              <td colspan="10"><input type="submit" value="Inscription"></td>
+
+    <div class="inscription">
+		
+        <form action="inscription.php" method="post">
+		
+        <tr>
+              <td colspan="10"><input type="submit" value="M'inscrire"></td>
         </tr>
-		</form>
-		</div>
+		
+        </form>
 	
-		
+    </div>
 	
-			
-		
-		
+    <div id="footer">
+
+        <p>Droit d'auteur © 2018 Nom du projet</p> 
+        <p> Dernière mise à jour le 30/04/2018 | 
+        <a href="mailto:nomDuProjet@gmail.com">nomDuProjet@gmail.com</a> 
+        </p>
+
+    </div>
 			
 </html>
--- indextraitement.php
+++ indextraitement.php
@@ -1,70 +1,66 @@
 ﻿<!DOCTYPE html>
 <html>
+
 <?php
 	$database = "piscine";
-	$db_handle = mysqli_connect('localhost', 'root', '');
+	$db_handle = mysqli_connect('localhost', 'root', 'root');
 	$db_found = mysqli_select_db($db_handle, $database);
-
-
-
 
     
     $email = isset($_POST["email"])?$_POST["email"] : "";
     $mdp = isset($_POST["mdp"])?$_POST["mdp"] : "";
-    $stat = "";
+    $error = "";
 
-    $error1 = "";
-    $error2 = "";
-    $error3 = "";
-	$error4 = "Combinaison email et mot de passe invalide";
-    $errorun = 0;
-    $errordeux = 0;
-    $connec = false;
+    if($email == ""){
+    	$error .= "Email est vide.";
+    }
+
+    //$stat = "";
+
+    //$error1 = "";
+    //$error2 = "";
+    //$error3 = "";
+	//$error4 = "Combinaison email et mot de passe invalide";
+    //$errorun = 0;
+    //$errordeux = 0;
+    //$connec = false;
 	
-    if($email == "") {$error2 = $error2." adresse mail -"; $errorun++;}
-    if($mdp == "") {$error2 = $error2." mot de passe "; $errorun++;}
+    //if($email == "") {$error2 = $error2." adresse mail -"; $errorun++;}
+    //if($mdp == "") {$error2 = $error2." mot de passe "; $errorun++;}
     
 
-    if($errorun > 0) {$error1 = "Il manque le champ : "; $error3 = "le remplir.";}
-    if($errorun > 1) {$error1 = "Il manque les champs : "; $error3 = "les remplir.";}    
-    if($errorun > 0)
-	{
-		Redirect('index.php?error_message='.$error1.$error2.'<br>Merci de '.$error3, false);
-	}
+    //if($errorun > 0) {$error1 = "Il manque le champ : "; $error3 = "le remplir.";}
+    //if($errorun > 1) {$error1 = "Il manque les champs : "; $error3 = "les remplir.";}    
+    //if($errorun > 0)
+	//{
+		//Redirect('index.php?error_message='.$error1.$error2.'<br>Merci de '.$error3, false);
+	//}
 	
 	// vérification de la connection
-	$sql = "SELECT mdp FROM utilisateur WHERE email = '$email'";
+	$sql = "SELECT mdp FROM utilisateur2 WHERE email = '$email'";
 	$MDP = mysqli_query($db_handle, $sql);
-	$resultat = $MDP;
-	$a = '$2y$10$QVZh6n7eiX6XLIrgaIpH8.4.vqpyDGCF2bfFfyY/ZwzCDFDbyVSUO';
 	
 	// Comparaison du mdp envoyé via le formulaire avec la base
-	$isPasswordCorrect = password_verify($mdp, $a);
+	//$isPasswordCorrect = password_verify($mdp, $a);
 	
-	if (!$resultat)
+	if ($MDP != $mdp)
 	{	
-		
 		echo 'Mauvais identifiant ou mot de passe !';
 	}
-	else
+
+	else if($mdp == $MDP)
 	{
-		
-		if ($isPasswordCorrect) {
-			//echo "vous etes connecter";
 		?>	
 			<meta http-equiv="refresh" content="1;sommaire.php"/>
 		<?php	
-			
-		}
-		else {
-			//echo 'votre mdp est '.$mdp;
-			Redirect('index.php?error_message='.'<br>email ou mot de passe faux ', false);
-		}
 	}
+
 	    function Redirect($url, $permanent = false)
     {
         header('Location: ' . $url, true, $permanent ? 301 : 302);
         exit();
     }
+    
 ?>
+
 </html>
