53b5cdd7f7b4445127c84f2a56d6afabd3d197a0
Vgiot14 <vgiot14@gmail.com>
2018-05-02 00:08:46 +0200

    Mise à jour sommaire php et css

--- sommaire.php
+++ sommaire.php
@@ -1,25 +1,30 @@
 <!DOCTYPE html>
+
 <html>
+
     <head>
         <title>Nom du projet</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<link href="sommairecss.css" rel="stylesheet" type="text/css" />
     </head>
-		<!-- Here is the menu area -->
-	 <header>
+
+	<header>
+
         <div id="titre">
             <h1>Nom du projet</h1>     
         </div>
+
 		<nav>
 			<ul>
-				<a href="acceuil.html">Acceuil </a>
-				<a href="reseau.html">Reseau </a>
-				<a href="emploi.html">Emploi </a>
-				<a href="messagerie.html">Messagerie </a>
-				<a href="notification.html">Notification </a>
-				<a href="profil.html">Profil </a>
+				<a href="acceuil.php">Accueil </a>
+				<a href="reseau.php">Reseau </a>
+				<a href="emploi.php">Emploi </a>
+				<a href="messagerie.php">Messagerie </a>
+				<a href="notification.php">Notification </a>
+				<a href="vous.php">Profil </a>
 			</ul>
 		</nav>
+
 	</header>
 			
 	<div class="publi">
@@ -32,6 +37,7 @@
 	
 
 	<section>
+
 	<p>ans La Forme d’une ville, Julien Gracq écrivait :
 
 2 J. Gracq, La Forme d’une ville, Paris, Corti, 1985, p. 2.
@@ -58,53 +64,20 @@ Habiter une ville, c’est y tisser par ses allées et venues journalières un l
 2.
 10Mais que faire alors de ce repérage, qui est en même temps plus de l’ordre de la focalisation sur un motif (dans la mesure où le texte lui-même le prend comme objet de la représentation) que de la reconstitution critique ? C’est ici que j’avancerai quelques propositions sur ce dont cet axe de lisibilité est le marqueur, afin de progresser dans l’élucidation d’une formule pouvant rendre compte du texte racinien. J’ajouterai que je n’irai guère plus loin dans le constat et qu’en tout cas, je m’abstiendrai d’en faire à mon tour une quelconque interprétation : j’essaierai tout au plus de remonter en amont, c’est-à-dire sur ce que ce constat implique quant au dispositif qui nous est donné à voir et à concevoir, à entendre et à comprendre.
 
-11Et pour le dire d’emblée, tout se passe comme si la mise en œuvre de la parole dans les textes de Racine s’effectuait en relation avec une idée générale concernant les fonctionnements discursifs, comme si le texte racinien jouait sur un horizon d’attente quant à ce que parler veut dire, bref, comme si la parole racinienne s’élaborait par rapport à ce qu’il est convenu d’appeler un modèle de lecture et d’interprétation de la parole oratoire. Plus précisément, notre première hypothèse sera la suivante : Racine prend comme règle du fonctionnement normal de la parole, le discours argumentatif à visée persuasive, c’est-à-dire un modèle qui associe la parole à la décision (à l’action) et fait de l’argumentation le moyen d’un engagement — qu’elle soit interpellation ou réplique, inaugurale ou en réponse, qu’elle permette d’accuser ou de défendre, de conseiller ou de mettre en garde, de blâmer ou de louer.
-
-12Après ce que nous venons de rappeler cependant un peu plus haut, concernant les démonstrations raciniennes d’impuissance oratoire, cette hypothèse peut paraître paradoxale si l’on ne va pas jusqu’au bout de sa logique. Que disons-nous en effet ? Rien de plus que ceci (mais rien de moins) : il existe un modèle de fonctionnement rhétorique de la parole comme discours efficace, finalisé sur l’action, modèle historiquement daté, commun à la fois au public et à l’auteur, et qui sert de norme (et de norme uniquement) pour la mobilisation et le repérage de la parole dans le corpus racinien. Et les parcours textuels se déploient alors par rapport à cette idée régulatrice, sur le mode de l’écartement et de la déception bien plus souvent que sur le mode de la fidélité (même s’il existe aussi des scènes qui maintiennent jusqu’au bout un fonctionnement rhétorique « normal », sans dénonciation ni congé : l’entrevue entre Burrhus et Néron, Acte IV, scène 3, en est un exemple évident). Nous ajouterons (simplement) que Racine non seulement construit la parole dramatique en référence à cette attente globalement rhétorique sur ce que parler veut dire (en jouant sur toute la gamme des modalités, de la fidélité à l’éloignement et à la contestation), mais qu’il lui arrive d’inscrire cette référence et sa contestation au cœur même de la fiction comme enjeu central d’un certain nombre de scènes pour cela prototypiques.
-
-13C’est donc de cela même qu’il est question dans les articles que l’on va lire. Le modèle lectorial du discours argumentatif persuasif a surtout été utilisé par la critique non pas pour établir le schéma des variations que la textualité racinienne lui fait subir, mais pour repérer et mettre en série (en réseau) les seuls cas de distanciation maximale, c’est-à-dire un ensemble d’éléments textuels qui s’en écartent jusqu’à converger dans le sens de l’invalidation du rhétorique :
-
-Au niveau de la scène (au sens d’unité inférieure à l’acte), on peut repérer tout ce qui, dans l’écoute et la réaction, échappe au pouvoir de l’argumentation (comme l’approfondit l’étude de Delphine Reguig) ;
-
-Au niveau inférieur de la tirade, il est également possible dès lors de repérer tout ce qui dans le dispositif argumentatif dysfonctionne et s’écarte de la composition attendue dans le genre discursif auquel il renvoie. On pense ici à la faiblesse ou même à l’absence des parties du discours prenant normalement en charge et explicitant le point de vue du destinataire, en particulier les exordes et les réponses aux objections (et nous renvoyons ici à l’article de Francis Goyet). Tant et si bien qu’il est tout aussi possible de voir les argumentaires comme des discours manqués que comme des machines oratoires virtuoses ; et qu’il est même légitime, par le déni et l’insouciance du destinataire dont témoignent ces dysfonctionnements maximaux, de plaider de façon plus radicale et dans des situations extrêmes comme celles qui ont été précisément répertoriées, pour une absence pure et simple de discours au sens rhétorique du terme, c’est-à-dire de dispositifs oratoires complets à visée persuasive (comme l’analyse Gilles Declercq quand il rappelle l’incomplétude fondamentale de la rhétoricité strictement textuelle).
-
-4 M. Charles, Introduction à l’étude des textes, Paris, Seuil, 1995, p. 283 sq.
-Au niveau supérieur de l’agencement même des actes enfin, des dysfonctionnements ont pu être analysés (je pense ici aux analyses de Michel Charles sur Bérénice4), qui vont dans le sens d’une pluralisation des structures : soit qu’ils partitionnent à l’intérieur des actes des sous-ensembles détournés de la progression argumentative, soit qu’ils confrontent des personnages ne communiquant pas dans des dispositifs de malentendus — et l’on songe bien évidemment, avec Delphine Denis, au malentendu terrible, irréductible, des acteurs humains ignorant le sens en dernière instance de l’imprécation d’Athalie (V, 6) prononcée sous le regard de Dieu.
-
-3.
-14On se souviendra alors que Charles voit dans ces écarts et ces ajustements un des ressorts du jeu structural que les classiques entretiennent avec la cohérence, avec l’idée d’unité. L’hypothèse est ici que le dysfonctionnement est le symptôme — on ne quitte décidément pas le registre du pathologique — d’une concurrence structurelle, et plus généralement d’un recoupement entre deux réseaux possibles, entre deux parcours et deux mises en convergence possibles des éléments textuels: il est un nœud et un carrefour. Ce qui suppose que les tirades, les scènes ou les ensembles qui font référence sur le mode de la dénégation au modèle de la parole rhétorique efficace, constituent une série qui s’organise également par rapport à un autre modèle possible, par rapport à une autre idée et une autre attente sur ce que la parole veut dire. Ce sera là notre deuxième proposition.
-
-5 Pour une synthèse sur l’exégèse juridique dans sa double acception d’interprétation grammaticale du (...)
-15Il me semble en effet qu’on peut tout autant défendre l’idée que la parole racinienne s’élabore et se perçoit par rapport à un modèle qui rapporte la parole à l’intention (et non à la décision) et fait de l’énoncé une exposition de soi. Tout se passe en effet comme si Racine prenait comme règle alternative pour le fonctionnement normal de la parole, la parole explicative à visée déclarative (qualificatif que je reprends aux juristes, au sens de déclaration d’intention). Si le premier modèle de la parole argumentative à visée persuasive s’ancrait historiquement dans la culture rhétorique, le second modèle s’ancre dans la culture exégétique (au sens où l’entend l’herméneutique juridique5), laquelle ramène la parole au couple littéral / intentionnel et inscrit à la fois la rédaction et la réception dans une stratégie de voilement et / ou de dévoilement de l’intention à l’origine du texte.
-
-16Si l’on reprend notre série de dysfonctionnements, il apparaît alors sans trop de difficultés que les trois scènes prototypiques que nous avons répertoriées voient explicitée et imposée par les destinataires la référence au modèle exégétique. Étéocle, Néron ou Monime questionnent l’intention à l’origine des paroles, ils interprètent la mobilisation d’une argumentation comme stratégie de leurre et de voilement des volontés. Même référence à ce modèle exégétique de la parole comme signe et même mise à l’écart du modèle rhétorique de la parole comme acte dans les tirades oratoires dysfonctionnelles : le déficit discursif des éléments liés à la visée pragmatique (exordes, réfutations) s’y accompagne d’une hypertrophie de la narratio : simple relation des faits qui peuvent aider à la conviction dans la rhétorique persuasive, la narratio devient dans les dispositifs raciniens de parole un élément clef de signification de soi, qu’elle soit au centre des tirades d’aveu (narrationes des circonstances où naquit l’amour, avec Néron en II, 2 ou Xipharès en I, 2…) ou au centre de tirades déclaratives politiques, où l’exposition (éminemment contrôlée) du moi est au service d’une imposition de sa puissance (narrationes auto-glorificatrices d’Agrippine en IV, 2 et plus encore de Mithridate dans son extraordinaire déclaration d’intention en III, 1). Même référence enfin à ce modèle de la parole et de l’être entier comme signes dans les scènes détournées de la progression persuasive, qu’elles soient confidences (dévoilement de l’intériorité) ou malentendus (divorce des intentionnalités et obscurité de la lettre des énoncés).
-
-17Deux modèles historiques viennent ainsi structurer le fonctionnement discursif du théâtre racinien: la parole comme acte, la parole comme signe. Ces deux modèles ouvrent chacun une voie d’entrée et de lisibilité, de convergence et de mise en réseau d’un certain nombre d’éléments du dispositif langagier ; c’est en référence à ces principes de régulation que s’établit une typologie des écarts et des variations ; enfin, leur chevauchement et leur recoupement donnent lieu à ces séries de dysfonctionnements, d’écartement maximal et de changement de modèle que nous n’avons cessé d’approfondir.
-
-4.
-18Mais pas tout à fait encore : il nous reste un dernier pas à franchir. Pour reprendre l’image gracquienne, je dirai que je viens d’établir deux réseaux possibles dans le fonctionnement de la parole racinienne, un premier réseau structuré autour du modèle rhétorique, un deuxième autour du modèle exégétique. Reste à se prononcer sur les modalités de leurs recoupements, de leurs chevauchements, de leurs relais, ce qui nous donnera précisément la forme même du texte, c’est-à-dire la formule selon laquelle il négocie ses changements d’équilibre et ajuste son dispositif.
-
-— Vous ? […] Vous l’aimez ? [Narcisse, Britannicus, II, 2]
-
-— Vous Seigneur ? […] Vous ! [Junie, Britannicus, II, 3]
-
-— Quoi le Roi… [Monime, Mithridate, V, 4]
-
-19De façon quasi caricaturale ici nous est donnée la formule des ajustements : non sur le mode musical de l’atténuation et de la progressivité, de la reprise et de l’abandon ; mais sur le mode direct et brutal de la rupture et de la surprise, du renversement littéralement étonnant, du coup de théâtre qui est aussi coup de tonnerre : ce qui marchait ne marche plus, le modèle instauré et dont on attendait la poursuite est d’un seul coup évacué. La formule de l’ajustement, c’est le paradoxe. Notre dernière proposition sera donc la suivante : Racine négocie la dynamique de sa structure textuelle selon un processus paradoxal qui consiste à susciter la référence à un modèle pour en établir l’attente et à établir l’attente pour mieux la décevoir.
-
-20Les modalités de cette formule paradoxale de l’ajustement structurel sont multiples : attente de la stratégie de persuasion et de son efficacité à l’orée des scènes dont l’enjeu argumentatif a été thématisé : et dénonciation paradoxale de l’intérêt argumentatif au nom d’une autre attente, qui prend le relais, concernant la lisibilité des intentions (La Thébaïde, IV, 3 ; Britannicus, IV, 2 ; Mithridate, IV, 4), concernant les effets de la théâtralité (Iphigénie, V, 4), concernant le déchiffrement d’une préfiguration christique (Athalie, V, 6). Attente, encore, du dispositif probatoire à l’orée des discours persuasifs (de Jocaste, Agrippine ou Mithridate) et déséquilibre des parties du discours (faiblesse de l’exorde et de la réfutation, hypertrophie de la narration…) qui reverse la parole dans un autre modèle, celui de la déclaration d’intention. Attente, enfin, d’un déroulement de l’histoire et d’une progression de l’intrigue (par mobilisation d’un canevas) et évacuation cavalière du canevas initié au profit d’un autre canevas au niveau de l’agencement et de l’ajustement des actes.
-
-21Je me contenterai de gloser ce dernier point pour finir : car je crois que c’est dans cette série des ajustements paradoxaux que l’on peut faire référence à l’art racinien de mobiliser différents modèles d’intrigues et à la façon rapide voire brutale dont ils sont habituellement congédiés. C’est ainsi que Mithridate, par son titre même (qui réfère à un personnage historique bien identifié au xviie siècle, comme ayant combattu jusqu’au bout contre l’envahisseur romain), instaure l’attente d’un canevas héroïque (le canevas du résistant glorieux dans sa défaite : pensée ingénieuse fécondant l’invention de l’intrigue, comme le montre l’article de Jean-Yves Vialleton), la déçoit dès l’incipit (« Mithridate est mort », v. 2), et mobilise ensuite d’autres canevas qui vont repousser et retarder jusqu’à l’acte V, scène 4, la mise en place du canevas que le titre laissait attendre. L’art racinien de l’intrigue suspendue passe par une pluralisation des canevas selon un schéma de progression par liquidation.
-
-5.
-22Pour finir tout à fait, je dirai qu’il n’y a pas une parole racinienne, s’élevant face à la nuit dans le silence, pas plus qu’il n’y a une intrigue systématique piégeant le héros dans une machine infernale ni un développement oratoire orchestré dans un discours monolithique : il n’y a que des cohérences momentanées, des dispositifs temporairement stabilisés, des régulations instables ; il y a surtout toute une technique de l’attentio déceptive, une façon de ne pas ajuster ni négocier autrement que par le renversement le passage d’un modèle régulateur à un autre, bref un art du mirabile que notre français traduit autant par admiration que par merveille.
-
 23La forme du texte racinien, c’est un jardin aux sentiers qui bifurquent.</p>
 	</section>
 	
 	
+	
 	</body>
 	
+    <div id="footer">
 
+        <p>Droit d'auteur © 2018 Nom du projet</p> 
+        <p> Dernière mise à jour le 30/04/2018 | 
+        <a href="mailto:nomDuProjet@gmail.com">nomDuProjet@gmail.com</a> 
+        </p>
+
+    </div>
 	
 </html>
